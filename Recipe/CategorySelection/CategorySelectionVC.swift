e2cf794e6b01d694c0f94e666342e115d36d2a28
Chris <chris.sedano.arenas@gmail.com>
2020-05-22 16:28:58 +0100

    CategoryVC
    implement DataSet to CategoryVC and configure cell's

---
+++ Recipe/CategorySelection/CategorySelectionVC.swift
@@ -0,0 +1,22 @@
+//
+//  CategorySelectionVC.swift
+//  Recipe
+//
+//  Created by Cristian Sedano Arenas on 22/05/2020.
+//  Copyright © 2020 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class CategorySelectionVC: UIViewController {
+    
+    var selectedCategory: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
--- Recipe/Controller/Category/CategoryVC.swift
+++ Recipe/Controller/Category/CategoryVC.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class CategoryVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = DataSet()
+    var categoryToPass : String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension CategoryVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as? CategoryCell {
+            
+            cell.configureCell(category: data.categories[indexPath.row])
+            
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        categoryToPass = data.categories[indexPath.row].title
+        performSegue(withIdentifier: "toSelection", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let recipeSelection = segue.destination as? CategorySelectionVC {
+            
+            recipeSelection.selectedCategory = categoryToPass
+        }
+    }
 }
